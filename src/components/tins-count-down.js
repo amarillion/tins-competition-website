482b4ba1524cae23eacd38ff63a7cbf97ab585ce
Martijn van Iersel <mvaniersel@gmail.com>
2020-12-04 15:56:29 +0100

    Make count-down dynamic

---
+++ src/components/tins-count-down.js
@@ -0,0 +1,96 @@
+import { LitElement, html, css } from 'lit-element';
+
+export class TinsCountDown extends LitElement {
+
+	static get properties() {
+		return {
+			epochMillis: { type: Number },
+			label: { type: String },
+			hidden: { type: Boolean, reflect: true },
+		};
+	}
+
+	constructor() {
+		super();
+		this.label = "";
+		this.hidden = false;
+		this.epochMillis = 0;
+	}
+
+	render() {
+		const d = new Date(this.epochMillis);
+		const now = Date.now();
+
+		const deltaSec = Math.floor((d - now) / 1000);
+		if (deltaSec < 0) return ''; // time in past is not displayed
+
+		const sec = deltaSec % 60;
+		const min = Math.floor(deltaSec / 60) % 60;
+		const hours = Math.floor(deltaSec / 3600) % 24;
+		const days = Math.floor(deltaSec / 86400);
+
+		let numbers, labels;
+		if (days > 0) {
+			numbers = [ days, hours ];
+			labels = [ "days", "hours" ];
+			setTimeout(() => this.requestUpdate(), 60*60*1000);
+		}
+		else 
+		if (hours > 0) {
+			numbers = [ hours, min ];
+			labels = [ "hours", "min" ];
+			setTimeout(() => this.requestUpdate(), 60*1000);
+		}
+		else {
+			numbers = [ min, sec ];
+			labels = [ "min", "sec" ];
+			setTimeout(() => this.requestUpdate(), 1000);
+		}
+
+		return html`
+			<div>${this.label}:</div> 
+			<div class="countdown">
+				<div class="numbers">${numbers[0]}</div><div class="labels">${labels[0]}</div>
+				<div class="numbers">${numbers[1]}</div><div class="labels">${labels[1]}</div>
+			</div>`;
+	}
+
+	static get styles() {
+		return css`
+			:host {
+				display: grid;
+				grid-template-columns: 1fr 1fr;
+				margin: 1rem 0;
+				justify-items: end;
+				align-items: center;
+			}
+
+			.countdown {
+				display: grid;
+				grid-auto-flow: column;
+				grid-template-columns: 4rem 4rem;
+				grid-template-rows: 3rem 1rem;
+			}
+
+			:host([hidden]) {
+				display: none;
+			}
+
+			.numbers {
+				background: #ff9000;
+				color: white;
+				margin: 4px;
+				text-align: center;
+				font-size: 32px;
+				box-shadow: 3px 3px 6px grey;
+				border-radius: 3px;
+				font-weight: bold;
+			}
+
+			.labels {
+				color: grey;
+				text-align: center;
+			}
+		`;
+	}
+}
--- src/components/tins-current-event.js
+++ src/components/tins-current-event.js
@@ -1,6 +1,9 @@
 import { LitElement, html, css } from 'lit-element';
 import { subscribe } from '../store';
 
+import { TinsCountDown } from './tins-count-down.js';
+customElements.define('tins-count-down', TinsCountDown);
+
 export class TinsCurrentEvent extends LitElement {
 
 	static get properties() {
@@ -35,49 +38,14 @@ export class TinsCurrentEvent extends LitElement {
 		this.unsubscribe.forEach(unsub => unsub());
 	}
 
-	renderIfFuture(label, epochMillis) {
-		
-		const d = new Date(epochMillis);
-		const now = Date.now();
-
-		const deltaSec = Math.floor((d - now) / 1000);
-		if (deltaSec < 0) return ''; // time in past is not displayed
-
-		const sec = deltaSec % 60;
-		const min = Math.floor(deltaSec / 60) % 60;
-		const hours = Math.floor(deltaSec / 3600) % 24;
-		const days = Math.floor(deltaSec / 86400);
-
-		let numbers, labels;
-		if (days > 0) {
-			numbers = [ days, hours ];
-			labels = [ "days", "hours" ];
-		}
-		else if (hours > 0) {
-			numbers = [ hours, min ];
-			labels = [ "hours", "min" ];
-		}
-		else {
-			numbers = [ min, sec ];
-			labels = [ "min", "sec" ];
-		}
-
-		return html`
-			<div>${label}:</div> 
-			<div class="countdown">
-				<div class="numbers">${numbers[0]}</div><div class="labels">${labels[0]}</div>
-				<div class="numbers">${numbers[1]}</div><div class="labels">${labels[1]}</div>
-			</div>`;
-	}
-
 	render() {
 		return html`
 			Current event:
 			<h1>${this.currentEvent.title}</h1>
 			<div class="datelist">
-			${this.renderIfFuture("Start", this.currentEvent.competitionStart)}
-			${this.renderIfFuture("Finish", this.currentEvent.competitionEnd)}
-			${this.renderIfFuture("Voting ends", this.currentEvent.votingEnd)}
+			<tins-count-down label="Start"       epochMillis=${this.currentEvent.competitionStart}></tins-count-down>
+			<tins-count-down label="Finish"      epochMillis=${this.currentEvent.competitionEnd}></tins-count-down>
+			<tins-count-down label="Voting ends" epochMillis=${this.currentEvent.votingEnd}></tins-count-down>
 			</div>
 			Already ${this.currentEvent.numEntrants} signed up. <a href="/join/" router-ignore>Click to join</a>!
 		`;
@@ -108,36 +76,6 @@ export class TinsCurrentEvent extends LitElement {
 			a:hover  { text-decoration: underline; }
 			a:active { text-decoration: underline; }
 
-			.datelist {
-				display: grid;
-				grid-template-columns: 1fr 1fr;
-				margin: 1rem 0;
-				justify-items: end;
-				align-items: center;
-			}
-
-			.countdown {
-				display: grid;
-				grid-auto-flow: column;
-				grid-template-columns: 4rem 4rem;
-				grid-template-rows: 3rem 1rem;
-			}
-
-			.numbers {
-				background: #ff9000;
-				color: white;
-				margin: 4px;
-				text-align: center;
-				font-size: 32px;
-				box-shadow: 3px 3px 6px grey;
-				border-radius: 3px;
-				font-weight: bold;
-			}
-
-			.labels {
-				color: grey;
-				text-align: center;
-			}
 		`;
 	}
 
