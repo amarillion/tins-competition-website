468fad6d19884f561a65778f754619372f797943
Martijn van Iersel <mvaniersel@gmail.com>
2021-02-13 18:15:31 +0100

    Fix typo

--- src/util.js
+++ src/util.js
@@ -7,7 +7,7 @@ import { dispatch } from './store.js';
 	Adds an event listener on a DOM element, and returns a function that 
 	removes it again when called.
 */
-export default function registerEventListener(elt, type, func, capture = false) {
+export function registerEventListener(elt, type, func, capture = false) {
 	elt.addEventListener(type, func, capture);
 	return () => {
 		elt.removeEventListener(type, func, capture);
