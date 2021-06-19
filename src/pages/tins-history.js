432eca7a1bfae5eb7d4cad877c4012be88f3a21d
Martijn van Iersel <mvaniersel@gmail.com>
2021-06-19 11:14:27 +0200

    Clean up history page

--- src/pages/tins-history.js
+++ src/pages/tins-history.js
@@ -1,7 +1,22 @@
 import { LitElement, html, css } from 'lit-element';
 import { ScopedElementsMixin } from '@open-wc/scoped-elements';
+import { repeat } from 'lit-html/directives/repeat.js';
+import { StoreSubscriberMixin } from '../data/storeSubscriberMixin.js';
+import { refreshCurrentEvent } from '../data/currentEvent.js';
+import { dispatch } from '../store.js';
 
-export class TinsHistory extends ScopedElementsMixin(LitElement) {
+export class TinsHistory extends StoreSubscriberMixin(ScopedElementsMixin(LitElement)) {
+
+	constructor() {
+		super();
+		this.events = [];
+	}
+
+	static get properties() {
+		return {
+			events: { type: Array },
+		};
+	}
 
 	static get styles() {
 		return css`
@@ -12,73 +27,50 @@ export class TinsHistory extends ScopedElementsMixin(LitElement) {
 		`;
 	}
 
+	get selectors() {
+		return {
+			events: s => s.currentEvent.data && s.currentEvent.data.events
+		};
+	}
+
+	connectedCallback() {
+		super.connectedCallback();
+		dispatch(refreshCurrentEvent());
+	}
+
 	render() {
+		const events = this.events;
+
 		return html`
-	<h1>History</h1>
+	<h2>TINS event history</h2>
+	
+	There have been game programming competitions on this site all the way back since 2003.
 
+	<div class="toc">
+	<ul>
+		${repeat(events, e => e.short, e => html`<li><a href="/${e.short}/" router-ignore>${e.title}</a></li>`)}
+	</ul>
+	</div>
 
+	<h2>Other community events</h2>
 	<p>
-	The allegro community has a long and rich history of wacky competitions. Take a look at this list. 
-	Many of these competitions still have a download section where you can download entries. Also, you can check out what the random additional requirements turned out to be in previous Speedhacks.
-	Possibly this will help you get an idea of what exactly this is all about.
+	The allegro community has a long and rich history of wacky competitions.
 	</p>
-	<p>
-	<a href="http://www.speedhack.allegro.cc/1999/">SpeedHack 1999</a>
-	</p>
-	<p>
-	<a href="http://www.speedhack.allegro.cc/2001/">SpeedHack 2001</a>
-	</p>
-	<p>
-	<a href="http://gamecode.tripod.com/sizehack3d.html">The Allegro SizeHack3D 2001</a>
-	</p>
-	<p>
-	<a href="http://amarillion.bafsoft.net/teamcompo/index.htm">The Allegro Team Competition 2001</a>
-	
-	</p>
-	<p>
-	<a href="http://binarysurge.netfirms.com/pgc/">The Pixelate Games Competition 2001</a>
-	</p>
-	<p>
-	<a href="http://www.hot.ee/ideahack/">IdeaHack 2001</a>
-	</p>
-	<p>
-	<a href="http://www.speedhack.allegro.cc/2002/">SpeedHack 2002</a>
-	</p>
-	<p>
-	<a href="http://www.binarysurge.com/blitzhack/">BlitzHack 2003</a>
-	</p>
-	
-	<p>
-	<a href="http://www.speedhack.allegro.cc/2003">Speedhack 2003</a>
-	</p>
-	<p>
-	<a href="http://amarillion.bafsoft.net/tins/">TINS 2003</a>
-	</p>
-	<p>
-	<a href="http://www.speedhack.allegro.cc/2004/">Speedhack 2004</a>
-	</p>
-	<p>
-	<a href="http://amarillion.bafsoft.net/tins05/">TINS 2005</a>
-	</p>
-	<p>
-	
-	<a href="http://www.speedhack.allegro.cc/2005/">Speedhack 2005</a>
-	</p>
-	<p>
-	<a href="http://amarillion.bafsoft.net/tins06/">TINS 2006</a>
-	</p>
-	<p>
-	<a href="http://www.speedhack.allegro.cc/2006/">Speedhack 2006</a>
-	</p>
-	<p>
-	<a href="http://amarillion.bafsoft.net/tins07/">TINS 2007</a>
-	</p>
-	<p>
-	<a href="http://www.speedhack.allegro.cc/2007/">Speedhack 2007</a>
-	</p>
-	<p>
-	<a href="http://amarillion.bafsoft.net/tins08/">TINS 2008</a>
-	</p>
+	<ol>
+	<li><a href="http://www.speedhack.allegro.cc/2007/">Speedhack 2007</a>
+	<li><a href="http://www.speedhack.allegro.cc/2005/">Speedhack 2005</a>
+	<li><a href="http://www.speedhack.allegro.cc/2004/">Speedhack 2004</a>
+	<li><!-- http://www.binarysurge.com/blitzhack/ -->BlitzHack 2003
+	<li><a href="http://www.speedhack.allegro.cc/2003">Speedhack 2003</a>
+	<li><a href="http://www.speedhack.allegro.cc/2002/">SpeedHack 2002</a>
+	<!-- Following Links unfortunately dead... -->
+	<li><!-- http://gamecode.tripod.com/sizehack3d.html -->The Allegro SizeHack3D 2001
+	<li><!-- http://amarillion.bafsoft.net/teamcompo/index.htm -->The Allegro Team Competition 2001	
+	<li><!-- http://binarysurge.netfirms.com/pgc/ -->The Pixelate Games Competition 2001
+	<li><!-- http://www.hot.ee/ideahack/ -->IdeaHack 2001
+	<li><a href="http://www.speedhack.allegro.cc/2001/">SpeedHack 2001</a>
+	<li><a href="http://www.speedhack.allegro.cc/1999/">SpeedHack 1999</a>
+	</ol>
 		`;
 	}
 }
