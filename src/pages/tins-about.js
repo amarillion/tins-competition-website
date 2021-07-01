761b6878919ce187eac24b1c922b8a30caca026e
Martijn van Iersel <mvaniersel@gmail.com>
2021-07-01 09:58:58 +0200

    Update about page

--- src/pages/tins-about.js
+++ src/pages/tins-about.js
@@ -14,44 +14,51 @@ export class TinsAbout extends ScopedElementsMixin(LitElement) {
 
 	render() {
 		return html`
-	<h1>About TINS - Game Jam</h1>
-
-	<h2>Do you have what it takes?</h2>
+	<h1>About this site...</h1>
 	<p>
-	Have you always wanted to make a game? Can you never find the time? Do you start but then get distracted by other stuff? Can you never find the focus to finish something?
+	This site is the home of two online Game Jams: <b>TINS</b> and <b>KrampusHack</b>. 
+	TINS is a speedhack, where you make a game in 72 hours. KrampusHack is a more relaxed game jam where you make a gift for somebody else, secret-santa style.
+	These game jams orginated in the <a href="https://www.allegro.cc">Allegro community</a>, but you may use any engine or programming framework you like.
 	</p>
+	<h2>About TINS</h2>
 	<p>
-	Here is your chance! Because here is TINS, a game jam where you can match your game programming skills against other fellow game programmers all around the world. Get your ideas out, get your game played. It is intense, but it is a lot of fun!
+	TINS is a 72 hour game programming competition. That means that you have to write a complete computer game <b>from scratch</b> in 
+	just one weekend! Sounds challenging, doesn't it? 
+	</p><p>
+	But wait, there is more! Right at the start of the competition, <b>additional rules</b> will be announced. 
+	These rules are picked at random by the <i>rule-o-matic</i>., and they may set limits on the genre of the game, 
+	require the implementation of a certain technical feat, or perhaps require 
+	something artistic addition. You have to be prepared for anything!
+	</p><p>
+	These extra rules always lead to a lot of creativity, see for example some of the submissions we got in 2020 in this video:
 	</p>
-	<h2>What is this is all about?</h2>
+	<iframe width="560" height="315" 
+		src="https://www.youtube.com/embed/3yR10fjSDtU" 
+		title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+	</iframe>
 	<p>
-	</p>
-	TINS is a 72 hour game programming competition. That means that you have to write a complete computer game <b>from scratch</b> in just one weekend! Sounds challenging, doesn't it? In fact, this is a very good way to learn a lot more about game programming. The short time span forces you to avoid needless fiddling, and really focus on what makes your game tick.
-	<p>
-	Here is the catch: there are a couple of <b>additional rules</b> that are not announced until the moment that the competition starts. In fact, these rules are picked at random by a device called the <i>rule-o-matic</i>, so not even the organizers know on beforehand what they are going to be. These rules may set limits on the genre of the game, require the implementation of a certain technical feat, or perhaps require something on the artistic side. You have to be prepared for anything! These extra rules are a safeguard to make sure nobody tries to cheat by working ahead before the start of the competition. One good strategy to deal with this is to plan a couple of small games that you would make, and select one of those ideas based on the additional rules.
-	</p>
-	<p>
-	There is one other thing you should know from the start: You have to use the <a href="http://alleg.sf.net/">allegro game programming library</a>, because this event is organized by and for the allegro community.
-	However, don't let this stop you if you don't know about Allegro! Allegro is really easy to learn, very flexible, and highly portable - it works on devices varying from mobile phones to the Raspberry Pi.
-	Allegro itself is written in C, but you can write your game in C++, Java, Python, D, Rust, Pascal and more.
-	</p>
-	<p>
-	There are some other <b>important rules</b>, such as a size limit and the requirement to publish source. See the rules page for each competition for details.
-	</p>
-	<p>
-	Please let it be known that we <b>welcome newbies</b> in all shapes and sizes. Don't be put off by the challenge! Maybe you won't win, but I guarantee you'll have a lot of fun, and learn a lot too! Your game will be seen and reviewed by many members of the allegro community.
-	</p>
-	<p>
+	There are some other <b>important rules</b>, such as the requirement to publish source. See the rules page for each competition for details. TINS is held every year in the summer.
+	</p><p>
+	We <b>welcome newbies</b> in all shapes and sizes. Don't be put off by the challenge! Maybe you won't win, but I guarantee you'll have a lot of fun, 
+	and learn a lot too! Your game will be seen and reviewed by members of the community. 
+	</p><p>
 	The Allegro Community has a <a href="/history">long history</a> of these kind of wacky game jams. Originally there was a series of events called "Speedhack". Some people found however, that one speedhack per year is not enough. That is why TINS came into existence,
 	it is basically a copy of the Speedhack event. This also explains the name: TINS is an acronym for "TINS Is Not Speedhack". It is, however, very much <i>like</i> Speedhack.
 	</p>
+	
+	<h2>About KrampusHack</h2>
 	<p>
-	Please go to the <a href="/accounts/register">registration page</a> if you want to join. Contact me at amarillion@yahoo.com if you have any questions.
+	KrampusHack works like a secret santa. During KrampusHack, each participant posts a wishlist of what they like to see in a game. 
+	At the start of the competition, you are assigned a random person and you have to make a game just for them, based on their wishlist.
 	</p>
+	<p>
+	Where TINS is a challenge of leet hacking skills, KrampusHack is much more relaxed. You get more time, there are fewer restrictions, and there is no voting, It's just a chance to hang out, 
+	have fun, be creative and do something nice for the community.
 	<h2>About the organizers</h2>
 	<p>
-	TINS is organized by <a href="http://www.helixsoft.nl">Amarillion</a>, who has been doing this kind of thing since 2001.
+	TINS is organized by <a href="http://www.helixsoft.nl">Amarillion</a>, who has been doing this kind of thing since 2001. Email me at amarillion@yahoo.com if you have any questions, or send me a message on twitter: <a href="https://twitter.com/mpvaniersel">@mpvaniersel</a>
 	</p>
+	
 		`;
 	}
 }
