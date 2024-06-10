01b662f6ff33af2d12bb1c99f4a535d0b4b7a158
Martijn van Iersel <mvaniersel@gmail.com>
2024-06-10 12:14:22 +0200

    Ensure serving static assets: vite's default is 'public'

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,5 @@ export default {
 	build: {
 		outDir: './static' // <- this is where djanog expects the static assets when running in local dev mode...
 	},
-	base: '/static/'
+	base: '/static/',
 };
