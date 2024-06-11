7fa5100a6c8120bff47e9f653e9c8477144fdfbf
Martijn van Iersel <mvaniersel@gmail.com>
2024-06-11 15:27:28 +0200

    Clean hidden files too. Re-enable compression

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite';
+import viteCompression from 'vite-plugin-compression';
 
 export default defineConfig(({ mode }) => {
 	return {
+		plugins: [
+			viteCompression()
+		],
 		build: {
 			outDir: './static', // <- this is where djanog expects the static assets when running in local dev mode...
 			sourcemap: mode === 'development',
