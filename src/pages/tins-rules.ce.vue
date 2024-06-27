8a912a4850f23cb644a32e5c56d2dc4eb82424e2
Martijn van Iersel <mvaniersel@gmail.com>
2024-06-27 12:27:46 +0200

    Vue-ify rules page

---
+++ src/pages/tins-rules.ce.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { fetchJSONOrThrow } from '../util.js';
+import { onMounted } from 'vue';
+import { usePromise } from '../usePromise.js';
+
+const m = window.location.pathname.match(`\/(?<compoId>[^\/]+)\/rules\/?$`);
+const { compoId } = m.groups;
+
+const breadcrumbs = [
+	{ title: compoId, url: `/${compoId}/` },
+	{ title: 'rules' },
+];
+
+const data = usePromise();
+onMounted(() => data.doAsync(async() => (await fetchJSONOrThrow(`/api/v1/compo/${compoId}/rules`)).result));
+
+</script>
+<template>
+	<tins-breadcrumbs :data="breadcrumbs"></tins-breadcrumbs>
+
+	<tins-status-helper :error="data.error.value" :loading="data.loading.value">
+		<div v-html="data.result.value"></div>
+	</tins-status-helper>
+</template>
+
+<style>
+	:host {
+		display: block; /* solves text selection issues */
+	}
+	pre {
+		white-space: pre-wrap;
+	}
+</style>
