04e4e0463ffa76abeaa23f4f39cf8fcce0a97518
Martijn van Iersel <mvaniersel@gmail.com>
2024-11-26 13:45:55 +0100

    Switch to vite-plugin-compression2, older one keeps crashing in dev mode

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite';
-import viteCompression from 'vite-plugin-compression';
+import compression from 'vite-plugin-compression2';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig(({ mode }) => {
 	return {
 		plugins: [
-			viteCompression(),
+			compression(),
 			vue(),
 		],
 		build: {
